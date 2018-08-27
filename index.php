52bbaa209fe81617e06af040d37a6e47464fe704
bkliepma <30784738+bkliepma@users.noreply.github.com>
2018-08-27 13:28:11 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>IOT Button</title>
+    </head>
+	
+	<body>
+	<a href="/cgi-bin/on.py">On</a> <br>
+	<a href="cgi-bin/off.py">Off</a>
+	</body>
+	
+</html>
