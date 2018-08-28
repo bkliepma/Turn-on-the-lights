5055b7dc8131fa49a3940d5c90440ce4f422f185
bkliepma <30784738+bkliepma@users.noreply.github.com>
2018-08-28 10:00:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
         <meta charset="utf-8" />
         <title>IOT Button</title>
     </head>
-	
+    
 	<body>
 	<a href="/cgi-bin/on.py">On</a> <br>
-	<a href="cgi-bin/off.py">Off</a>
+	<a href="cgi-bin/off.py">Off</a> <br>
+    	<a href="/cgi-bin/Sunrise.py">Sunrise</a> <br>
+	<a href="cgi-bin/Sunset.py">Sunset</a> <br>
+    	<a href="/cgi-bin/reset.py">Reset</a> <br>
+	<a href="cgi-bin/pause.py">Pause</a> <br>
 	</body>
 	
 </html>
