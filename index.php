6a7671c2cdafd000746e6888b8f3b0986470d843
bkliepma <30784738+bkliepma@users.noreply.github.com>
2018-08-30 11:38:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Bedroom Lights</title>
+        <title>IOT Button</title>
     </head>
     
 	<body>
@@ -12,20 +12,16 @@
     	<a href="/cgi-bin/Sunrise.py">Sunrise</a> <br>
 	<a href="cgi-bin/Sunset.py">Sunset</a> <br>
     	<a href="/cgi-bin/reset.py">Reset</a> <br>
-	<a href="cgi-bin/pause.py">Pause</a> <br>
+	<a href="cgi-bin/pause.py">Pause</a> 
 	#https://stackoverflow.com/questions/39671105/have-link-appear-only-if-href-file-exists
+	#http://php.net/manual/en/control-structures.if.php
+	#TEST THIS
 	<?php
-	echo '<ul>';
 	$pausefile = 'PauseFile.txt';
-                if (file_exists($pausefile)) {
-                $file_clean = explode('.', $pausefile);
-                $file_clean = str_replace('-', ' ', $pausefile_clean[0]);
-                $file_clean = ucfirst($file_clean);
-                echo '<li><a class="button" href="' . $pausefile . '">' . $file_clean . '</a></li>';
-                }
-            }
-	echo '</ul>';
-	?>
+	if (file_exists($pausefile)): ?>
+            <a href="cgi-bin/pausefile.txt">Paused</a><br>
+        <?php else: ?>
+        #Display blank
 	</body>
 </html>
 
