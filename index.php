b50871103e4c38fe560094b02a4b21d4d69ab233
bkliepma <30784738+bkliepma@users.noreply.github.com>
2018-08-30 11:32:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>IOT Button</title>
+        <title>Bedroom Lights</title>
     </head>
     
 	<body>
@@ -13,6 +13,19 @@
 	<a href="cgi-bin/Sunset.py">Sunset</a> <br>
     	<a href="/cgi-bin/reset.py">Reset</a> <br>
 	<a href="cgi-bin/pause.py">Pause</a> <br>
+	#https://stackoverflow.com/questions/39671105/have-link-appear-only-if-href-file-exists
+	<?php
+	echo '<ul>';
+	$pausefile = 'PauseFile.txt';
+                if (file_exists($pausefile)) {
+                $file_clean = explode('.', $pausefile);
+                $file_clean = str_replace('-', ' ', $pausefile_clean[0]);
+                $file_clean = ucfirst($file_clean);
+                echo '<li><a class="button" href="' . $pausefile . '">' . $file_clean . '</a></li>';
+                }
+            }
+	echo '</ul>';
+	?>
 	</body>
-	
 </html>
+
